8352c2d92a07e0385f3462a6a339c7a3a4334203
Sina Hajikazemi <sina.hkazemi@gmail.com>
2024-03-17 16:26:57 +0100

    image file path changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ exclude_patterns = []
 # html_theme = 'alabaster'
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-html_logo = "_static/logo.svg"
+html_logo = "logo.svg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
