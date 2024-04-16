287121022093ebf49f70383d13c404e7797820b9
Sina Hajikazemi <sina.hkazemi@gmail.com>
2024-04-16 11:46:28 +0200

    excessive files removed

--- attack/data_access.py
+++
@@ -1,7 +0,0 @@
-from Core.data_access import DAO
-from sqlite3 import Connection
-
-class AttackDAO(DAO):
-    def __init__(self, conn: Connection) -> None:
-        super().__init__(conn)
-        self.output_index_dict["Upper"] = ['T']
