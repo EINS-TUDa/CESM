7bad868b25899bbe45c624898c5b5eb82c45945a
Sina Hajikazemi <sina.hkazemi@gmail.com>
2024-04-17 15:20:26 +0200

    version in setup.py corrected

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cesm',
-    version='0.0.1',
+    version='v0.0.1alpha.0',
     packages=find_packages(),
     package_data={
         'cesm': ['Data/*'],
