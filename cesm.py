322281cc6a1779f00eec399b5fa5099ea6d7b682
Sina Hajikazemi <sina.hkazemi@gmail.com>
2024-04-17 19:31:07 +0200

    init added to setup file

--- cesm.py
+++ cesm.py
@@ -8,49 +8,52 @@ import click
 import os
 import time 
 from pathlib import Path
-
+from distutils.dir_util import copy_tree
+import pkg_resources
 from InquirerPy import prompt
 
 # -- internal imports -- #
 from Core.input_parser import Parser
 from Core.model import Model
-# from Core.datacls import save_input_output, read_input_output
 from Core.plotter import Plotter, PlotType
 from Core.data_access import DAO
 import sqlite3
 
 # Constants
+DATA_DIR_PATH = Path(".").joinpath('Data')
 TECHMAP_DIR_PATH = Path(".").joinpath('Data', 'Techmap')
 TS_DIR_PATH = Path(".").joinpath('Data', 'TimeSeries')
 RUNS_DIR_PATH = Path(".").joinpath('Runs')
+
 FNAME_MODEL = 'db.sqlite'
 # -- Helpers -- #
 def get_existing_models():
       """Return a list of existing models"""
-      files = os.listdir(TECHMAP_DIR_PATH)
+      # files = os.listdir(TECHMAP_DIR_PATH)
+      files = [file.name for file in TECHMAP_DIR_PATH.glob("*") if file.is_file()]
       return [tm.split('.')[0] for tm in files if tm.endswith('.xlsx')]
 
-class ModelChoice(click.Choice):
-    def convert(self, value, param, ctx):
-         try:
-               return super().convert(value, param, ctx)
-         except click.exceptions.BadParameter:
-               # print each model in one line   
-               available_models = '\n\t '.join(get_existing_models())
-               raise click.BadParameter(f"Model {value} not found.\n \tAvailable models are:\n\t {available_models}")
+# class ModelChoice(click.Choice):
+#     def convert(self, value, param, ctx):
+#          try:
+#                return super().convert(value, param, ctx)
+#          except click.exceptions.BadParameter:
+#                # print each model in one line   
+#                available_models = '\n\t '.join(get_existing_models())
+#                raise click.BadParameter(f"Model {value} not found.\n \tAvailable models are:\n\t {available_models}")
 
 def get_runs():
    """Return a list of existing runs"""
-   return os.listdir(RUNS_DIR_PATH)
+   return [entry.name for entry in RUNS_DIR_PATH.iterdir() if entry.is_dir()]
 
-class RunsChoice(click.Choice):
-   def convert(self, value, param, ctx):
-      try:
-         return super().convert(value, param, ctx)
-      except click.exceptions.BadParameter:
-         # print each model in one line   
-         available_runs = '\n\t '.join(get_runs())
-         raise click.BadParameter(f"Run {value} not found.\n \tComputed model runs are:\n\t {available_runs}")
+# class RunsChoice(click.Choice):
+#    def convert(self, value, param, ctx):
+#       try:
+#          return super().convert(value, param, ctx)
+#       except click.exceptions.BadParameter:
+#          # print each model in one line   
+#          available_runs = '\n\t '.join(get_runs())
+#          raise click.BadParameter(f"Run {value} not found.\n \tComputed model runs are:\n\t {available_runs}")
 
 def get_list_inquirer_choices(choices, name=None, message=None, type='list'):
    """Return a dict of choices in the format required by PyInquirer"""
@@ -61,15 +64,15 @@ def get_list_inquirer_choices(choices, name=None, message=None, type='list'):
       'choices': choices,
    }
 
-def prompt_commodities(doa):
-      cos = [str(c) for c in doa.get_set("commodity")]
+def prompt_commodities(dao):
+      cos = [str(c) for c in dao.get_set("commodity")]
       # remove Dummy
       cos.remove('Dummy')
 
       return prompt(get_list_inquirer_choices(cos, name='commodities', type='checkbox', message='Select commodities: Select with spacebar and confirm with enter'))['commodities']
 
-def prompt_years(doa):
-    yy = [int(y) for y in doa.get_set("year")]
+def prompt_years(dao):
+    yy = [int(y) for y in dao.get_set("year")]
     return prompt(get_list_inquirer_choices(yy, name='years', type='checkbox', message='Select years: Select with spacebar and confirm with enter'))['years']
    
 
@@ -77,19 +80,19 @@ def prompt_years(doa):
 @click.group()
 def app():
    """Welcome to the Compact Energy System Modelling Tool (CESM)"""
-   
+
 @app.command(name='run')
-@click.option("--model_name",'-m', type=ModelChoice(get_existing_models()))
-@click.option("--scenario",'-s', type=click.STRING)
-def run(model_name, scenario):
+# @click.option("--model_name",'-m', type=ModelChoice(get_existing_models()))
+# @click.option("--scenario",'-s', type=click.STRING)
+def run():
    """Run the Model"""
-   print(f'Running model {model_name} with scenario {scenario}')
+   # print(f'Running model {model_name} with scenario {scenario}')
 
    # If no scenario or model is provided, prompt the user to choose one
-   if model_name is None:
-      model_name = prompt(get_list_inquirer_choices(get_existing_models(), name='model_name', message='Please choose a model to run'))['model_name']
-   if scenario is None:
-      scenario = click.prompt('Please enter a scenario name', type=click.STRING)
+   # if model_name is None:
+   model_name = prompt(get_list_inquirer_choices(get_existing_models(), name='model_name', message='Please choose a model to run'))['model_name']
+   # if scenario is None:
+   scenario = click.prompt('Please enter a scenario name', type=click.STRING)
 
    # Create a directory for the model if it does not exist
    db_dir_path = RUNS_DIR_PATH.joinpath(model_name+'-'+scenario)
@@ -141,14 +144,11 @@ def run(model_name, scenario):
    print(f"Saving model finished in {time.time()-st:.2f} seconds")
      
 @app.command(name='plot')
-@click.option("--simulation", '-s', type=RunsChoice(get_runs()), help="Name of the simulation to visualize. If not provided user will be prompted to choose one.")
-def plot(simulation):
+def plot():
    """Visualize the results of a simulation"""
 
-   if simulation is None:
-      simulation = prompt(get_list_inquirer_choices(get_runs(), name='simulation', message='Please choose a simulation to visualize'))['simulation'] 
+   simulation = prompt(get_list_inquirer_choices(get_runs(), name='simulation', message='Please choose a simulation to visualize'))['simulation'] 
       
-
    print(f"Visualizing simulation {simulation}")
    db_path = os.path.join(RUNS_DIR_PATH, simulation, FNAME_MODEL)
    conn = sqlite3.connect(db_path)
@@ -202,7 +202,15 @@ def plot(simulation):
    
    click.echo("Plotting finished!")
 
+@app.command(name='init')
+def initialize():
+   print("Initializing CESM...")
+   RUNS_DIR_PATH.mkdir(exist_ok=True)
+   if not DATA_DIR_PATH.exists():
+      DATA_DIR_PATH.mkdir()
+      data_folder_path = pkg_resources.resource_filename('cesm', 'Data')
+      copy_tree(data_folder_path, str(Path(".").joinpath("Data").absolute()))
+   print("Data files are successfully structured.")
 
 if __name__ == "__main__":
    app()
-   pass
--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='cesm',
-    version='v0.0.1alpha.0',
+    version='0.0.2',
     packages=find_packages(),
+    description='Compact Energy System Modeling Tool (CESM)',
+    long_description="""
+Compact Energy System Modeling Tool (CESM)
+This is a compact energy system modeling tool that can model different forms of energy carriers and the conversion processes that convert them to each other. 
+The optimal output of the model determines how much should be invested in each part of the energy system to meet the energy demand and minimize costs.
+
+An example of the German energy system is also provided. The results of the model are compatible with the results of the paper "Barbosa, Julia, Christopher Ripp, and Florian Steinke. Accessible Modeling of the German Energy Transition: An Open, Compact, and Validated Model. Energies 14, no. 23 (2021)"
+    """,
     package_data={
         'cesm': ['Data/*'],
     },
+    long_description_content_type='text/markdown',
     py_modules=['cesm'],  # Assuming cesm.py is in the root of your package directory
     entry_points={
         'console_scripts': [
@@ -29,7 +38,6 @@ setup(
     # Metadata
     author=['Sina Hajikazemi','Julia Barbosa'],
     author_email='sina.hkazemi@email.com',
-    description='Compact Energy System Modeling Tool',
     url='https://github.com/EINS-TUDa/CESM',
     license='MIT',
 )
